e7450baadc8b6b0a57cb553e4a8232de494fee78
sacrtap <89307033@qq.com>
2026-03-03 20:39:54 +0800

    feat: create customer list and placeholder pages

---
+++ frontend/src/views/system/UserManage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="user-manage">
+    <Card title="用户管理">
+      <div>用户管理页面开发中...</div>
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.user-manage {
+  padding: 24px;
+}
+</style>
